76d8edc317ca13becc0cc2b6cde7226a06fd2bfc
Igor <igorlipko75@gmail.com>
2018-08-05 11:17:12 +0300

    Whitelisting form fields for binding products

--- src/main/java/my/webstore/controller/ProductController.java
+++ src/main/java/my/webstore/controller/ProductController.java
@@ -5,6 +5,10 @@ import my.webstore.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -55,18 +59,29 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/products/add", method = RequestMethod.GET)
-/*
-    public String getAddNewProductForm(Model model) {
-        Product newProduct = new Product();
-        model.addAttribute("newProduct", newProduct);
-*/
-        public String getAddNewProductForm(@ModelAttribute("newProduct") Product newProduct) {
+    /*
+     * public String getAddNewProductForm(Model model) { Product newProduct = new Product();
+     * model.addAttribute("newProduct", newProduct);
+     */
+    public String getAddNewProductForm(@ModelAttribute("newProduct") Product newProduct) {
         return "addProduct";
     }
 
     @RequestMapping(value = "/products/add", method = RequestMethod.POST)
-    public String processAddNewProductForm(@ModelAttribute("newProduct") Product newProduct) {
+    public String processAddNewProductForm(@ModelAttribute("newProduct") Product newProduct,
+            BindingResult result) {
+        String[] suppressedFields = result.getSuppressedFields();
+        if (suppressedFields.length > 0) {
+            throw new RuntimeException("Attempting to bind disallowed fields: "
+                    + StringUtils.arrayToCommaDelimitedString(suppressedFields));
+        }
         productService.addProduct(newProduct);
         return "redirect:/market/products";
+    }
+
+    @InitBinder
+    public void initialiseBinder(WebDataBinder binder) {
+        binder.setAllowedFields("productId", "name", "unitPrice", "description", "manufacturer",
+                "category", "unitsInStock", "condition");
     }
 }
